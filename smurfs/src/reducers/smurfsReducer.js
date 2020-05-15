79c36b34880328e67c41c501aa048f11648fd00d
unknown <prakashbahadursingh2@gmail.com>
2020-05-15 10:16:54 -0700

    initial state and empty  reducer function created in thho smurfReducer file

---
+++ smurfs/src/reducers/smurfsReducer.js
@@ -0,0 +1,32 @@
+
+
+const initialState = [
+    {
+      name: "Brainey",
+      age: 200,
+      height: "5cm",
+      id: 0
+    }
+  ];
+
+
+export const smurfReducer = (state = initialState, action) => {
+
+    switch(action.type) {
+
+
+
+
+
+       
+       
+       
+       
+       
+       
+       
+       
+        default:
+            return state;
+    }
+}
